93a63af882010c3275da25b18125c3dfd502e856
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-06-18 08:13:43 +0700

    update settings.py

--- datxe_backend/settings.py
+++ datxe_backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hethongdatxe',
         'USER': 'root',
-        'PASSWORD': 'Abc123!@#',
+        'PASSWORD': 'Hungtran2003.',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
